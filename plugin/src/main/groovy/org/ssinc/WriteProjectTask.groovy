a8e6a869872a1456035b2a314cff37ff4f6be168
Stuart Sinclair <aeternuscreperum@gmail.com>
2019-06-04 22:32:09 +0100

    Wiring up the plugin nice and simples

--- plugin/src/main/groovy/org/ssinc/WriteProjectTask.groovy
+++ plugin/src/main/groovy/org/ssinc/WriteProjectTask.groovy
@@ -3,18 +3,23 @@ package org.ssinc
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
+import org.ssinc.projectwriter.ProjectWriter
 
 import javax.inject.Inject
 
 class WriteProjectTask extends DefaultTask {
 
+    private Project project
+
     @Inject
     WriteProjectTask(Project project) {
         description = 'Write the project to a build.gradle file'
+        this.project = project
     }
 
     @TaskAction
     def writeProject() {
-
+        project.logger.info("Writing build.gradle file to $project.rootDir")
+        ProjectWriter.write(project, project.rootDir)
     }
 }
